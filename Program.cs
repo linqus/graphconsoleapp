94fef22da98ff857e1b61af75a2060c6f870f963
Karol Linkiewicz <karol@linkiewicz.pl>
2022-08-07 12:58:31 +0200

    settings loader

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace graphconsoleapp
     public class Program
     {
         private static GraphServiceClient? _graphClient;
+
+        private static IConfigurationRoot? LoadAppSettings()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", false, true)
+                                .Build();
+
+                if (string.IsNullOrEmpty(config["applicationId"]) ||
+                    string.IsNullOrEmpty(config["applicationSecret"]) ||
+                    string.IsNullOrEmpty(config["redirectUri"]) ||
+                    string.IsNullOrEmpty(config["tenantId"]))
+                {
+                return null;
+                }
+
+                return config;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+        }
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
